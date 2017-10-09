bca61a763f8a4bc7054f52dec7ebd13dd2f9d750
Lars Hopman <l.k.hopman@gmail.com>
2017-10-09 13:39:47 +0200

    Implemented revoke customer mandate functionality

---
+++ src/Message/RevokeCustomerMandateResponse.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Date: 18/08/17
+ * Time: 12:44
+ */
+
+namespace Omnipay\Mollie\Message;
+
+
+class RevokeCustomerMandateResponse extends AbstractResponse
+{
+    public function isSuccessful()
+    {
+        return parent::isSuccessful();
+    }
+}
